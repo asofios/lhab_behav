3b8763d694f9ea7e4f13c5e64b8c21324f127253
fliem <franz.liem@gmail.com>
2020-05-18 09:14:37 +0200

    removed tap and wts

--- scripts/aggregate_tp6/aggregate_psychometric_tp6.py
+++ scripts/aggregate_tp6/aggregate_psychometric_tp6.py
@@ -9,8 +9,6 @@ out_path.mkdir(exist_ok=True)
 files = list(root_path.glob("lhab*.xlsx"))
 
 df_pp_out = pd.DataFrame()
-df_tap_out = pd.DataFrame()
-df_wts_out = pd.DataFrame()
 
 
 def format_test_sheet(df, fill_colls=['Domain', 'Test'], score_col="Score_Name",
@@ -46,25 +44,5 @@ for f in files:
     df_pp_ = pd.concat((id, pp), axis=1, sort=False)
     df_pp_out = df_pp_out.append(df_pp_, sort=False)
 
-    # 3. TAP
-    tap = pd.read_excel(f, sheet_name="TAP")
-    tap = format_test_sheet(tap, fill_colls=['Test'], score_col="Score_Name", var_cols=['Test', 'Score_Name'])
-    tap["file"] = f
-    df_tap_ = pd.concat((id, tap), axis=1, sort=False)
-    df_tap_out = df_tap_out.append(df_tap_, sort=False)
-
-    # 4. WTS
-    wts = pd.read_excel(f, sheet_name="WTS")
-    wts = format_test_sheet(wts, fill_colls=['Test'], score_col="Score_Name", var_cols=['Test', 'Score_Name'])
-    wts["file"] = f
-    df_wts_ = pd.concat((id, wts), axis=1, sort=False)
-    df_wts_out = df_wts_out.append(df_wts_, sort=False)
-
 df_pp_out.reset_index(drop=True, inplace=True)
 df_pp_out.to_excel(out_path / "00_aggregated_PP.xlsx", index=False)
-
-df_tap_out.reset_index(drop=True, inplace=True)
-df_tap_out.to_excel(out_path / "00_aggregated_TAP.xlsx", index=False)
-
-df_wts_out.reset_index(drop=True, inplace=True)
-df_wts_out.to_excel(out_path / "00_aggregated_WTS.xlsx", index=False)
