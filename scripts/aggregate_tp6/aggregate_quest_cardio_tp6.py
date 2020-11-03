304c75d7c9bc49e88d1fdc8b4385604a7bea7a13
fliem <franz.liem@gmail.com>
2020-11-03 16:48:33 +0100

    initial

--- scripts/aggregate_tp6/aggregate_quest_cardio_tp6.py
+++ scripts/aggregate_tp6/aggregate_quest_cardio_tp6.py
@@ -16,14 +16,24 @@ def excel_letter_to_num(l):
     return letter_lut[l.lower()]
 
 
-def extract_data_via_mapping(file, lut, sheet="01_Veränderungsfragebogen", row_offset=-2):
+def extract_data_via_mapping(file, lut27, lut29, sheet="01_Veränderungsfragebogen", row_offset=-2):
     df_out = pd.DataFrame()
-    lut = lut.dropna(axis="index", how="all")
 
     # encoding breaks load code
     sheet_ = 4 if sheet == "03_Kardivaskulär" else sheet
 
     df_in = pd.read_excel(file, sheet_name=sheet_)
+
+    # cardio comes in different formats 29 and 27 lines
+    # (this is because some files dont have the Keine der genannten Behandlungen cells
+
+    if len(df_in) == 29:
+        lut = lut29.dropna(axis="index", how="all")
+    elif len(df_in) == 27:
+        lut = lut27.dropna(axis="index", how="all")
+    else:
+        raise Exception(file, len(df_in))
+
     for _, row in lut.iterrows():
         name, col_idx, row_idx = row["variable_short_engl"], excel_letter_to_num(row["value_col"]), \
                                  int(row["value_row"]) + row_offset
@@ -33,23 +43,25 @@ def extract_data_via_mapping(file, lut, sheet="01_Veränderungsfragebogen", row_
     return df_out
 
 
-sheets = ["02_Gesundheitszustand", "10_Soziale_Unterstützung", "12_Pittsburgh_Sleep_Inventory"]
+sheet = "03_Kardivaskulär"
 
-for sheet in sheets:
-    print(sheet)
-    lut_file = Path(f"/Volumes/lhab_public/03_Data/04_data_questionnaires/00_rawdata_tp6/mapping/mapping_{sheet}.xlsx")
-    lut = pd.read_excel(lut_file)
+lut_file = Path(
+    f"/Volumes/lhab_public/03_Data/04_data_questionnaires/00_rawdata_tp6/mapping/mapping_{sheet}_27.xlsx")
+lut_27 = pd.read_excel(lut_file)
+lut_file = Path(
+    f"/Volumes/lhab_public/03_Data/04_data_questionnaires/00_rawdata_tp6/mapping/mapping_{sheet}_29.xlsx")
+lut_29 = pd.read_excel(lut_file)
 
-    dfs = []
-    for f in files:
-        id = pd.read_excel(f, sheet_name="ID", usecols="A:B", names=["variable", "value"], header=None)
-        id.dropna(axis="index", how="all", inplace=True)
-        id = id.set_index("variable").T
-        id["file"] = f
+dfs = []
+for f in files:
+    id = pd.read_excel(f, sheet_name="ID", usecols="A:B", names=["variable", "value"], header=None)
+    id.dropna(axis="index", how="all", inplace=True)
+    id = id.set_index("variable").T
+    id["file"] = f
 
-        df1 = extract_data_via_mapping(f, lut, sheet=sheet)
-        df = pd.concat((id, df1), axis=1)
-        dfs.append(df)
+    df1 = extract_data_via_mapping(f, lut_27, lut_29, sheet=sheet)
+    df = pd.concat((id, df1), axis=1)
+    dfs.append(df)
 
-        df_out = pd.concat(dfs, axis=0, sort=False)
-        df_out.to_excel(out_path / f"00_aggregated_{sheet}.xlsx", index=False)
+    df_out = pd.concat(dfs, axis=0, sort=False)
+    df_out.to_excel(out_path / f"00_aggregated_{sheet}.xlsx", index=False)
